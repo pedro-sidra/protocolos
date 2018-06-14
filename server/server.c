3cd4a50a344d56e5aba387d888406bb6acbea35c
ENG10048 Protocolos de Comunicacao - A - Grupo 2 <eng10048_2@kolmogorov.ece>
2018-06-14 15:31:21 -0300

    aula 14/6.. desculpa commit ruim

--- server/server.c
+++ server/server.c
@@ -12,8 +12,12 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdbool.h>
-
+#include <time.h>
+#include <math.h>
 #include <pthread.h>
+
+#define DEBUG true
+#define SIM_TS_DEFAULT 10
 
 int getComandoArgumento(char*str, char*cmd,int* arg);
 int kbhit(void);
@@ -22,11 +26,13 @@ void handleMensagem(char* msg,char* retorno);
 void *comms();
 void *simPlanta();
 void initPlanta();
+double saturate(double val, double upper, double lower);
 
-struct tanque{
+struct planta{
 	double nivel;
-	double influx;
-	double outflux;
+	struct timespec simTS;
+	double comandoValvula;
+	double MAX;
 };
 
 pthread_mutex_t mutexPlanta = PTHREAD_MUTEX_INITIALIZER;
@@ -34,8 +40,9 @@ pthread_mutex_t mutexPlanta = PTHREAD_MUTEX_INITIALIZER;
 int argc =0;
 char **argv;
 bool end = false;
-struct tanque planta;
-double simTS = 1;
+
+struct planta pl;
+
 
 int main(int argcl,char *argvl[])
 {
@@ -59,33 +66,100 @@ int main(int argcl,char *argvl[])
 	 fprintf(stderr,"Error simthread - pthread_create() return code: %d\n",iret2);
 	 exit(EXIT_FAILURE);
 	}
-	
 	while(!kbhit());
 	end = true;
-
 	pthread_join( commthread, NULL);
 	pthread_join( simthread, NULL); 
 
 	exit(EXIT_SUCCESS);
 	return 0;
 }
+
+// PLANTA:
 void initPlanta()
 {
-	planta.nivel = 0.4;
-	planta.influx = 0;
-	planta.outflux=0;
+	pl.nivel = 0.4;
+	pl.MAX=100;
+	pl.comandoValvula=0;
+	pl.simTS.tv_sec  = 0;
+	pl.simTS.tv_nsec = SIM_TS_DEFAULT*1000000L;
+}
+struct valvula {
+	double angleNow;
+	double angleNext;
+};
+
+struct level{
+	double now;
+	double next;
+};
+double saturate(double val, double lower, double upper)
+{
+	if(val > upper)
+		val = upper;
+	else if(val < lower)
+		val = lower;
 }
 void *simPlanta()
 {
-	int i;
+	double dT = SIM_TS_DEFAULT;
+	double delta = 0;
+	double influx, outflux;
+	struct valvula in, out;
+	struct level lv;
+	lv.now = 0.4;
+	lv.next = 0.4;
+	in.angleNow = 0;
+	in.angleNext = 0;
+	out.angleNow = 0;
+	out.angleNext = 0;
+	printf("%f",dT);
+	
 	while(!end)
 	{
 		pthread_mutex_lock( &mutexPlanta );
-		planta.nivel+=0.01;
+		out.angleNow = saturate(out.angleNext,0,100);
+		in.angleNow = saturate(in.angleNext,0,100);
+		lv.now = saturate(lv.next,0,1);
+		pl.nivel=lv.now*100;
+		dT = (double)pl.simTS.tv_nsec/1000000.0;
+		printf("%f",dT);
+		//Rotina de simulacao:
+		if (pl.comandoValvula!=0) {
+			delta   += (pl.comandoValvula);
+			pl.comandoValvula = 0;
+		}
+		if (delta > 0) {
+			if(delta < 0.02*dT) 
+			{
+				in.angleNext=  in.angleNow+delta;
+				delta = 0; 
+			}else
+			{
+				in.angleNext=  in.angleNow+0.02*dT;
+				delta -=  0.02*dT;
+			}
+		} else if (delta < 0) {  
+			if(delta > -0.02*dT) {
+				in.angleNext= in.angleNow+delta;
+				delta = 0;
+			} 
+			else {
+				in.angleNext=  in.angleNow-0.02*dT;
+				delta +=  0.02*dT;
+			}
+		}
+
+		influx = 1*sin(M_PI/2*in.angleNow/100);
+		outflux= (pl.MAX/100.0)*(lv.now/1.25+0.2)*sin(M_PI/2*out.angleNow/100);
+		lv.next=lv.now+0.00001*dT*(influx-outflux);
+		
+		//Fim da rotina de simulacao
 		pthread_mutex_unlock( &mutexPlanta );
-		sleep(simTS);
+		nanosleep(&pl.simTS, NULL);
 	}
 }
+// COMUNICAÇÃO:
 void *comms()
 {
      int sockfd, newsockfd, portno;
@@ -141,64 +215,66 @@ void handleMensagem(char* msg, char* retorno)
 	int argumento;
 	
 	getComandoArgumento(msg,comando,&argumento);
-	printf("argumento: %d \n",argumento);
-	printf("comando: %s \n",comando);
-	
+	if(DEBUG)
+	{
+		printf("argumento: %d \n",argumento);
+		printf("comando: %s \n",comando);
+		time_t t = time(NULL);
+		struct tm tm = *localtime(&t);
+		printf("now: %d-%d-%d %d:%d:%d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+	}
 	pthread_mutex_lock( &mutexPlanta );
+	
+	// ABRE VALVULA
 	if(!strcmp("abreValvula",comando))
 	{
-		sprintf(retorno,"Angulo da valvula: %d",argumento);	
-	}else if(!strcmp("fechaValvula",comando))
+		pl.comandoValvula = saturate(argumento,0,100);
+		sprintf(retorno,"Variacao no angulo da valvula: %d",argumento);	
+		
+	}
+	// FECHA VALVULA 
+	else if(!strcmp("fechaValvula",comando))
 	{
-		sprintf(retorno,"Angulo da valvula: %d",argumento);
-	}else if(!strcmp("getNivel",comando))
+		pl.comandoValvula = -saturate(argumento,0,100);
+		sprintf(retorno,"Variacao no angulo da valvula: - %d",argumento);
+	}
+	// GET NIVEL 
+	else if(!strcmp("getNivel",comando))
 	{
-		argumento = (int) 100*planta.nivel;
+		argumento = (int)pl.nivel;
 		sprintf(retorno,"Nivel do tanque: %d",argumento);
-	}else if(!strcmp("testaConexao",comando))
+	}
+	// TESTA CONEXAO
+	else if(!strcmp("testaConexao",comando))
 	{
 		strcpy(retorno,"OK");
-	}else if(!strcmp("setPeriodoSimulacao",comando))
+	}
+	// SET PERIODO SIMULACAO
+	else if(!strcmp("setPeriodoSimulacao",comando))
 	{
-		simTS=argumento/1000;
-		sprintf(retorno,"Periodo de sim setado: %d ms",argumento);
-	}else if(!strcmp("setConsumo",comando))
+		long tvnew = (long)argumento*1000L*1000L;
+		if(tvnew < 999999999L && tvnew>0)
+		{
+			pl.simTS.tv_nsec=tvnew;
+			sprintf(retorno,"Periodo de sim setado: %d ms",argumento);
+		}else
+		{
+			sprintf(retorno,"Periodo de sim muito grande! Try again...");
+		}
+	}
+	// SET CONSUMO
+	else if(!strcmp("setConsumo",comando))
 	{
+		pl.MAX = saturate(argumento,0,100);
 		sprintf(retorno,"Consumo setado: %d",argumento);
-	}else if(!strcmp("iniciaSimulacao",comando))
+	}
+	// INICIA SIMULACAO
+	else if(!strcmp("iniciaSimulacao",comando))
 	{
 		strcpy(retorno,"OK");
 	}
 	pthread_mutex_unlock( &mutexPlanta );
 }
-
-int kbhit(void)
-{
-  struct termios oldt, newt;
-  int ch;
-  int oldf;
- 
-  tcgetattr(STDIN_FILENO, &oldt);
-  newt = oldt;
-  newt.c_lflag &= ~(ICANON | ECHO);
-  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-  oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
-  fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
- 
-  ch = getchar();
- 
-  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-  fcntl(STDIN_FILENO, F_SETFL, oldf);
- 
-  if(ch != EOF)
-  {
-    ungetc(ch, stdin);
-    return 1;
-  }
- 
-  return 0;
-}
-
 int getComandoArgumento(char*str, char*cmd,int* arg)
 {
 	char* phash;
@@ -249,5 +325,35 @@ void error(const char *msg)
     perror(msg);
     exit(1);
 }
+
+// MISC:
+
+int kbhit(void)
+{
+  struct termios oldt, newt;
+  int ch;
+  int oldf;
+ 
+  tcgetattr(STDIN_FILENO, &oldt);
+  newt = oldt;
+  newt.c_lflag &= ~(ICANON | ECHO);
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+  oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+  fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+ 
+  ch = getchar();
+ 
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+  fcntl(STDIN_FILENO, F_SETFL, oldf);
+ 
+  if(ch != EOF)
+  {
+    ungetc(ch, stdin);
+    return 1;
+  }
+ 
+  return 0;
+}
+
 
 
